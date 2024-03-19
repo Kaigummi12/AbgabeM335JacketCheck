1a1a6e5c0c48d5e9fe628c5cc3ed9b74f9a5a49c
Kaigummi12 <kaiwenninger1@gmail.com>
2024-03-19 08:13:55 +0100

    Edited mainActivity file

--- app/src/main/java/com/example/abgabem335jacketcheck/MainActivity.java
+++ app/src/main/java/com/example/abgabem335jacketcheck/MainActivity.java
@@ -1,24 +1,36 @@
 package com.example.abgabem335jacketcheck;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText nameEditText;
+    Button submitButton;
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        nameEditText = findViewById(R.id.nameEditText);
+        submitButton = findViewById(R.id.submitButton);
+
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name = nameEditText.getText().toString();
+                Intent intent = new Intent(MainActivity.this, WetterPage.class);
+                intent.putExtra("NAME", name);
+                startActivity(intent);
+            }
         });
     }
 }
